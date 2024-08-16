e98925221c2442cb33aa7574a861f366895156e3
BernieCodez <166939245+BernieCodez@users.noreply.github.com>
2024-08-15 20:38:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(__dirname));
 app.get('/app', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 });
